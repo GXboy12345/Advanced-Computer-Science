45973b3b89dfa3f7b0c5a7be184e3dbe51749489
GXboy12345 <garretmorbergnguyen@gmail.com>
2025-08-28 12:24:14 -0700

    using doubles instead of float

--- Day 0/VexStorageBox.java
+++ Day 0/VexStorageBox.java
@@ -3,15 +3,15 @@ import java.util.Map;
 public class VexStorageBox {
     public static final String SIZE = "Small";
     public String capacity = "Full";
-    public float weight = 1.5f; // pounds
+    public Double weight = 1.5; // pounds
 
-    public final Map<String, Float> contents;
+    public final Map<String, Double> contents;
 
-    public VexStorageBox() {
+    private VexStorageBox() {
         contents = Map.of(
-            "Ethernet Cable 1", 1f,
-            "Ethernet Cable 2", 2f,
-            "Ethernet Cable 3", 3f
+            "Ethernet Cable 1", 1.0,
+            "Ethernet Cable 2", 2.0,
+            "Ethernet Cable 3", 3.0
         );
     }
 }
