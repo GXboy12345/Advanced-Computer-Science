135e9566f726d7f40d53aa0304932db8a043c81b
GXboy12345 <garretmorbergnguyen@gmail.com>
2025-08-28 13:16:36 -0700

    Conditional adding, enhancements of logic

--- Day 0/VexStorageBox.java
+++ Day 0/VexStorageBox.java
@@ -1,23 +1,40 @@
-import java.util.Map;
 import java.util.Arrays;
+
 public class VexStorageBox {
-    private static String SIZE;
-    private String capacity;
+    private static int SIZE; //max items
+    private int capacity; //stored items
     private Double weight; // pounds
     private String[] contents;
 
     public VexStorageBox() {
-        this.contents = null;
-        capacity = "Empty";
+        this.contents = new String[0];
+        capacity = 0;
         weight = 1.5;
-        SIZE = "Small";
+        SIZE = 5;
     }
 
     public void inspectContents() {
-        System.out.println(this.contents);
+        System.out.println((Arrays.toString(contents)));
+    }
+
+    public void checkCapacity() {
+        System.out.println(Integer.toString(capacity));
     }
 
     public void trashContents() {
-    this.contents = new String[0];
+        this.contents = new String[0];
+    return;
+    }
+
+    public void addItem(String item) {
+        if(capacity+1 <= SIZE) {
+            this.contents = Arrays.copyOf(this.contents, this.contents.length + 1);
+            this.contents[this.contents.length - 1] = item;
+            capacity = this.contents.length;
+            return;
+        }
+        else {
+            System.out.println("The box is already full!");
+        }
     }
 }
--- Day 0/VexStorageBoxTester.java
+++ Day 0/VexStorageBoxTester.java
@@ -1,10 +1,25 @@
 public class VexStorageBoxTester {
     public static void main() {
-        System.out.println("Lorem Ipsum Dolor Sit Amet");
+        // System.out.println("Lorem Ipsum Dolor Sit Amet");
 
         VexStorageBox ethernetCables = new VexStorageBox();
+        ethernetCables.inspectContents();
+
+        ethernetCables.addItem("Some item");
+        ethernetCables.checkCapacity();
+        ethernetCables.inspectContents();
+
+        ethernetCables.trashContents();
+        ethernetCables.inspectContents();
+
+        for(int i = 1; i < 6; i++) {
+            ethernetCables.addItem("Trash item #" + Integer.toString(i));
+        }
+        ethernetCables.addItem("Test");
+        ethernetCables.inspectContents();
     }
-    public int multiply(int a, int b) {
-        return a*b;
-    }
+
+    // public int multiply(int a, int b) {
+    //     return a*b;
+    // }
 }
