da09503933a4ea9011d5fb56afd12809e9542f75
GXboy12345 <garretmorbergnguyen@gmail.com>
2025-08-28 12:39:12 -0700

    Tester class init

--- Day 0/VexStorageBox.java
+++ Day 0/VexStorageBox.java
@@ -1,17 +1,19 @@
 import java.util.Map;
 
 public class VexStorageBox {
-    public static final String SIZE = "Small";
-    public String capacity = "Full";
-    public Double weight = 1.5; // pounds
+    private static String SIZE;
+    private String capacity;
+    private Double weight; // pounds
+    private final Map<String, Double> contents;
 
-    public final Map<String, Double> contents;
-
-    private VexStorageBox() {
+    public VexStorageBox() {
         contents = Map.of(
             "Ethernet Cable 1", 1.0,
             "Ethernet Cable 2", 2.0,
             "Ethernet Cable 3", 3.0
         );
+        capacity = "Empty";
+        weight = 1.5;
+        SIZE = "Small";
     }
 }
---
+++ Day 0/VexStorageBoxTester.java
@@ -0,0 +1,5 @@
+public class VexStorageBoxTester {
+    public static void main() {
+        System.out.println("Lorem Ipsum Dolor Sit Amet");
+    }
+}
